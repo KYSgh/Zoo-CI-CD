b9d45ce62e56fb34773a6f2108166b3d8a264b05
changki123 <101972075+changki-123@users.noreply.github.com>
2024-03-22 04:17:05 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,7 @@ mkdir -p webb
 mv ./app/my-zootopic-front-main /home/ec2-user/webb
 mv ./app/reactDockerfile /home/ec2-user/webb
 cd /home/ec2-user/webb
+mv ./reactDockerfile ./Dockerfile
 sudo docker build -t reacts-webb .
 sudo docker run -d -p 3000:3000 reacts-webb
 
